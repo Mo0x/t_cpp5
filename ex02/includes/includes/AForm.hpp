a3595e73a7a01902cafd478d5226c2d80695c69d
Mo0x <max.govindaraju@gmail.com>
2025-01-16 16:59:28 +0100

    ex02

---
+++ ex02/includes/includes/AForm.hpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AForm.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgovinda <mgovinda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/14 17:12:37 by mgovinda          #+#    #+#             */
+/*   Updated: 2025/01/16 16:59:04 by mgovinda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AFORM_HPP
+#define AFORM_HPP
+
+#include <iostream>
+#include "Bureaucrat.hpp"
+
+class Bureaucrat;
+
+class AForm
+{
+	private :
+		std::string	m_name;
+		bool		m_signed;
+		const int	m_sign_req;
+		const int	m_exec_req;
+
+	public :
+		AForm();
+		AForm(std::string name, int sign_req, int exec_req);
+		AForm(const AForm &src);
+		AForm &operator=(const AForm &src);
+		~AForm();
+	//get set
+		std::string get_name(void) const;
+		int			get_sign_req(void) const;
+		int			get_exec_req(void) const;
+		bool		get_signed(void) const;
+		void		set_name(std::string name);
+		void		set_signed(bool sign);
+		void		beSigned(Bureaucrat &bureaucrat);
+
+	class GradeTooLowException : public std::exception
+	{
+		public:
+			virtual const char *what() const throw();
+	};
+	class GradeTooHighException : public std::exception
+	{
+		public:
+			virtual const char * what() const throw();
+	};
+};
+
+std::ostream &operator<<(std::ostream &o, AForm const &a);
+
+#endif
---
+++ ex02/includes/includes/Bureaucrat.hpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgovinda <mgovinda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/18 17:22:10 by mgovinda          #+#    #+#             */
+/*   Updated: 2025/01/16 16:57:49 by mgovinda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+#include <iostream>
+#include "AForm.hpp"
+
+class AForm;
+
+class Bureaucrat
+{
+	private:
+	std::string const	m_name;
+	int					m_grade;
+
+	public:
+	Bureaucrat();
+	Bureaucrat(int grade);
+	Bureaucrat(std::string name);
+	Bureaucrat(std::string name, int grade);
+	Bureaucrat(const Bureaucrat &src);
+	Bureaucrat &operator=(const Bureaucrat &src);
+	~Bureaucrat();
+
+	//get set
+	std::string	get_name(void) const;
+	int			get_grade(void) const;
+	void		set_grade(int grade);
+
+	//member fun
+	void		increase_grade(void);
+	void		decrease_grade(void);
+	void		signForm(AForm &form);
+	
+	//Exceptions
+	class GradeTooLowException : public std::exception
+	{
+		public:
+			virtual const char *what() const throw();
+	};
+	class GradeTooHighException : public std::exception
+	{
+		public:
+			virtual const char * what() const throw();
+	};
+};
+
+std::ostream &operator<<(std::ostream &o, Bureaucrat const &a);
+
+#endif
