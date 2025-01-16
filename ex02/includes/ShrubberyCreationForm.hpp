b1db4b35eaae5d11f74e4a55eadbe63899b7afd9
Mo0x <max.govindaraju@gmail.com>
2025-01-16 17:08:52 +0100

    ex02

---
+++ ex02/includes/ShrubberyCreationForm.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ShrubberyCreationForm.hpp                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgovinda <mgovinda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/16 17:07:20 by mgovinda          #+#    #+#             */
+/*   Updated: 2025/01/16 17:08:46 by mgovinda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SHRUBBERYCREATIONFORM_HPP
+#define SHRUBBERYCREATIONFORM_HPP
+
+#include "AForm.hpp"
+#include "Bureaucrat.hpp"
+#include <iostream>
+
+class ShurbberyCreationForm : public AForm
+{
+
+};
+
+#endif
