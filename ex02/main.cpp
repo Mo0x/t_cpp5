3794783aea6b00dabac3798e91048902f2d0f4eb
Mo0x <max.govindaraju@gmail.com>
2025-01-17 17:46:20 +0100

    start of presidential pardon

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mgovinda <mgovinda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 18:45:32 by mgovinda          #+#    #+#             */
-/*   Updated: 2025/01/17 16:49:19 by mgovinda         ###   ########.fr       */
+/*   Updated: 2025/01/17 17:43:07 by mgovinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include "AForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 int main(void)
 {
@@ -39,5 +40,20 @@ int main(void)
 		bl.signForm(robot);
 		bl.executeForm(robot);
 
+	}
+	{
+		PresidentialPardonForm pardon_me("Moi");
+		Bureaucrat noob("fucking noob", 145);
+		Bureaucrat med("wageslave", 10);
+		Bureaucrat boss("Capone", 1);
+
+		std::cout << pardon_me << noob << med << boss << std::endl; 
+		noob.signForm(pardon_me);
+		noob.executeForm(pardon_me);
+		med.signForm(pardon_me);
+		med.executeForm(pardon_me);
+		boss.executeForm(pardon_me);
+
+		std::cout << pardon_me << std::endl;
 	}
 }
